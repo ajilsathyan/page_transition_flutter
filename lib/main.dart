40f22127dbefbb9d12619b95984cf8ba9a1b1a46
ajilsathyan <ajilsathyan1998@gmail.com>
2021-08-11 16:42:56 +0530

    'second'

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -27,71 +30,181 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Screen1 extends StatelessWidget {
-  const Screen1({Key? key}) : super(key: key);
+class Screen1 extends StatefulWidget {
+  @override
+  _Screen1State createState() => _Screen1State();
+}
+
+class _Screen1State extends State<Screen1> {
+  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Screen"),),
-      body: Center(
-
-        child: TextButton(
-          onPressed: (){
-            showDialog(context: context, builder: (_){
-              return Dialog(
-                child: Container(
-                  height: 100,
-                  width: 300,
-                  child:ElevatedButton(
-                    onPressed: (){
-                      Navigator.of(context).push(_createRoute());
-                    },
-                    child: Text("Next Screen"),
-                  ) ,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.teal,
+        title: Text("Home Screen"),
+        actions: [
+          ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.teal),
+              ),
+              onPressed: () {
+                setState(() {
+                  isSelected = true;
+                });
+              },
+              child: Text("Filters",
+                  style: TextStyle(color: Colors.white, fontSize: 18)))
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: SingleChildScrollView(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  itemCount: 30,
+                  itemBuilder: (_, i) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 6, vertical: 4),
+                      child: Card(
+                        elevation: 2,
+                        shadowColor: Colors.pink,
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 80,
+                          margin: EdgeInsets.only(left: 5),
+                          child: Row(
+                            children: [
+                              Text("My name is ${i.toString()}"),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ),
+          if (isSelected)
+            Positioned(
+              right: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width - 170,
+                height: MediaQuery.of(context).size.height - 50,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blueAccent),
+                    color: Colors.greenAccent),
+                child: Stack(
+                  children: [
+                    SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            height: 40,
+                          ),
+                          ExpansionTile(
+                            title: Text('Range'),
+                            children: [
+                              ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: BouncingScrollPhysics(),
+                                  itemCount: 5,
+                                  itemBuilder: (_, i) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 25),
+                                      child: ListTile(
+                                        title: Text("Title ${i.toString()}"),
+                                      ),
+                                    );
+                                  }),
+                            ],
+                          ),
+                          ExpansionTile(
+                            title: Text("Doctor Category"),
+                            children: [
+                              ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: BouncingScrollPhysics(),
+                                  itemCount: 5,
+                                  itemBuilder: (_, i) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 25),
+                                      child: ListTile(
+                                        title: Text("Title ${i.toString()}"),
+                                      ),
+                                    );
+                                  }),
+                            ],
+                          ),
+                          ExpansionTile(
+                            title: Text('Others'),
+                            children: [
+                              ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: BouncingScrollPhysics(),
+                                  itemCount: 5,
+                                  itemBuilder: (_, i) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 25),
+                                      child: ListTile(
+                                        title: Text("Title ${i.toString()}"),
+                                      ),
+                                    );
+                                  }),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      left: 0,
+                      top: 10,
+                      child: Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  isSelected = false;
+                                });
+                              },
+                              icon: Icon(Icons.close))
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-              );
-            });
-
-          },
-          child: Text("Next Screen 2"),
-        ),
+              ),
+            ),
+        ],
       ),
     );
   }
 }
 
-
-
-
-Route _createRoute() {
-  return PageRouteBuilder(
-    transitionDuration: Duration(seconds: 1),
-    reverseTransitionDuration: Duration(seconds: 1),
-    pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-
-      return SlideTransition(
-
-        position: Tween<Offset>(
-          begin: Offset(1,0),
-          end: Offset.zero
-        ).animate(animation),
-        child: child,
-      );
-    },
-  );
-}
-
-class Page2 extends StatelessWidget {
-  const Page2({Key? key}) : super(key: key);
-
+class CustomContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Page 2'),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height - 300,
+            color: Colors.transparent,
+          )
+        ],
       ),
     );
   }
